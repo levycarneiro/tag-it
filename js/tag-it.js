89573259ae737a420aec13c916cbb5bfb2b64052
Aaron Severs <asevers@gmail.com>
2011-05-07 12:00:53 -0700

    Changed "tag_input" to "var tag_input" to avoid trouble putting more than one of these tag fields on a single page due to the global declaration.

--- js/tag-it.js
+++ js/tag-it.js
@@ -16,7 +16,7 @@
 		var html_input_field = "<li class=\"tagit-new\"><input class=\"tagit-input\" type=\"text\" /></li>\n";
 		el.html (html_input_field);
 
-		tag_input		= el.children(".tagit-new").children(".tagit-input");
+		var tag_input		= el.children(".tagit-new").children(".tagit-input");
 
 		$(this).click(function(e){
 			if (e.target.tagName == 'A') {
@@ -72,7 +72,7 @@
 
 		function is_new (value){
 			var is_new = true;
-			this.tag_input.parents("ul").children(".tagit-choice").each(function(i){
+			tag_input.parents("ul").children(".tagit-choice").each(function(i){
 				n = $(this).children("input").val();
 				if (value == n) {
 					is_new = false;
@@ -87,9 +87,9 @@
 			el += "<a class=\"close\">x</a>\n";
 			el += "<input type=\"hidden\" style=\"display:none;\" value=\""+value+"\" name=\"item[tags][]\">\n";
 			el += "</li>\n";
-			var li_search_tags = this.tag_input.parent();
+			var li_search_tags = tag_input.parent();
 			$(el).insertBefore (li_search_tags);
-			this.tag_input.val("");
+			tag_input.val("");
 		}
 	};
 
